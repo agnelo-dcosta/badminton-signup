a2cc3bd780e3513a280955dd914fe11f34bceeea
Agnelo Dcosta <agnelo.dcosta@warnermedia.com>
2024-05-09 12:12:07 -0700

    post login

--- run.py
+++ run.py
@@ -16,10 +16,23 @@ for url in urls:
 
 XPATH_username='/html/body/form/div[3]/table/tbody/tr[4]/td/div/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[3]/td/input'
 XPATH_password='/html/body/form/div[3]/table/tbody/tr[4]/td/div/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[5]/td/input'
+XPATH_login = '/html/body/form/div[3]/table/tbody/tr[4]/td/div/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[8]/td/input'
+
 driver.find_element(By.XPATH,XPATH_username).send_keys('agnelo.dcosta')
 sleep(2)
 driver.find_element(By.XPATH,XPATH_password).send_keys('July2017!')
 sleep(2)
+driver.find_element(By.XPATH,XPATH_login).click()
+sleep(5)
+
+XPATH_fixed = '/html/body/form/div[3]/table/tbody/tr[4]/td/div/h3/table/tbody/tr[3]/td/label'
+XPATH_enter = '/html/body/form/div[3]/table/tbody/tr[4]/td/div/h3/input'
+driver.find_element(By.XPATH,XPATH_fixed).click()
+sleep(2)
+driver.find_element(By.XPATH,XPATH_enter).click()
+
+XPATH_date = '/html/body/form/div[3]/table/tbody/tr[4]/td/div/div/select'
+driver.find_element(By.XPATH,XPATH_date)
 
 sleep(1000)
 
